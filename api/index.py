26f4f31cc4521cec84c7bd77f8f5a4c31a60194c
unknown <junma0614@gmail.com>
2025-04-05 00:39:23 +0800

    updated index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from .grc import app
 from asgiref.wsgi import WsgiToAsgi
+from flask import Flask
 
 if not isinstance(app, Flask):
     raise TypeError("Expected Flask app instance but got something else.")
